89092321334fb6dca197bb0f23b4cfd7120a7445
Jason Larkin <larkinjason123@gmail.com>
2025-10-02 15:51:16 +0000

    Dashboard card Pending samples makes no sense since samples will be ente

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -3,37 +3,18 @@
 
 import { Header } from '@/components/header';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
-import { activeExceedances, samples } from '@/lib/data';
+import { activeExceedances } from '@/lib/data';
 import { ActiveExceedances } from '@/components/dashboard/active-exceedances';
 import { OverviewChart } from '@/components/dashboard/overview-chart';
 import Link from 'next/link';
-import { Users, Briefcase, AlertTriangle, FlaskConical } from 'lucide-react';
+import { Users, Briefcase, AlertTriangle } from 'lucide-react';
 
 export default function DashboardPage() {
-  const pendingSampleCount = samples.filter(s => {
-      const result = s.result;
-      return !result || result.status === 'Pending';
-  }).length;
-
   return (
     <div className="flex min-h-screen w-full flex-col">
       <Header title="Dashboard" />
       <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
-        <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
-          <Card>
-            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-              <CardTitle className="text-sm font-medium">
-                Pending Samples
-              </CardTitle>
-              <FlaskConical className="h-4 w-4 text-muted-foreground" />
-            </CardHeader>
-            <CardContent>
-              <div className="text-2xl font-bold">{pendingSampleCount}</div>
-              <p className="text-xs text-muted-foreground">
-                Samples awaiting lab analysis.
-              </p>
-            </CardContent>
-          </Card>
+        <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-3">
           <Card>
             <Link href="/personnel">
                 <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">

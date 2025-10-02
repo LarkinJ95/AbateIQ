8ba4e2364767444092b67518d3d2d2769d512a0b
Jason Larkin <larkinjason123@gmail.com>
2025-10-02 15:35:55 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2248828915-b3814",
+  "appId": "1:960555879799:web:062a99ab4e51567e2be572",
+  "apiKey": "AIzaSyCK72mZTN3p_9mkl3OpY854Ryy2mHM95bM",
+  "authDomain": "studio-2248828915-b3814.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "960555879799"
+};

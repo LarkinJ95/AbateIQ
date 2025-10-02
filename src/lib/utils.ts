e9e43cb7501d52c5c4b188401420ac782ac64011
Jason Larkin <larkinjason123@gmail.com>
2025-10-02 19:40:47 +0000

    Try fixing this error: `Console Error: `DialogContent` requires a `Dialo

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,14 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+// You can add this to your existing utils.ts file or a new one
+export function visuallyHidden(
+  ...inputs: ClassValue[]
+): string {
+  return cn(
+    'sr-only',
+    'absolute w-px h-px p-0 -m-px overflow-hidden clip-rect(0, 0, 0, 0) whitespace-nowrap border-0',
+    ...inputs
+  );
+}

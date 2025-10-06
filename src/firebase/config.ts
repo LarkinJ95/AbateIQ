1c79c740b939f4b2773220b1a3696c3ded4f5aaa
Jason Larkin <larkinjason123@gmail.com>
2025-10-06 15:18:25 +0000

    Try fixing this error: `Runtime Error: FirebaseError: Firebase: Error (a

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,8 @@
+
 export const firebaseConfig = {
   "projectId": "abateiq",
   "appId": "1:933446077259:web:968593406f082e16a7b744",
-  "apiKey": "AIzaSyAZf-15b533e4S8uT5696dCg1u0Ff5p_4",
+  "apiKey": "AIzaSyAZf-l5b533e4S8uT5696dCg1u0Ff5p_4",
   "authDomain": "abateiq.firebaseapp.com",
   "measurementId": "G-L0W621DE03",
   "storageBucket": "abateiq.appspot.com"
